2a8b21710cc324a20a5ed6733449e79d3f6f59f9
Boris Quiroz <boris@insert-coin.org>
2020-10-12 19:39:13 -0300

    Remove copy option (not needed), add help messages and short format for each option

--- src/ngen/cli.py
+++ src/ngen/cli.py
@@ -9,13 +9,19 @@ def main():
     return True
 
 @main.command()
-@click.option('--secret', default='/')
-@click.option('--db', '-d', default=db)
+@click.option('--secret', '-s', default='/', help='Secret name')
+@click.option('--db', '-d', default=db, help='DB to read the secrets from')
 def list(secret, db):
+    """
+    List secrets in current DB
+    """
     subprocess.run([cmd, 'ls', db, secret])
 
 @main.command()
-@click.option('--copy', default='')
-@click.option('--key', default='')
-def copy(copy, key):
+@click.option('--key', '-k', default='', help='Name of the secret to copy to clipboard')
+@click.option('--db', '-d', default=db, help='DB to copy the secrets from')
+def copy(key, db):
+    """
+    Copy selected secret
+    """
     subprocess.run([cmd, 'clip', db, key])
