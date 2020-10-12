ef1a462c2ee7fdba3d6d0efd5e3cda767cf258ec
Boris Quiroz <boris@insert-coin.org>
2020-10-12 13:04:38 -0300

    initial setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme=f.read()
 
 setup(
-        name='Passwd Fetcher',
-        version='1.1',
-        py_modules=['pass'],
-        install_requires=[
-            'Click',
-            ],
-        entry_points='''
-            [console_scripts]
-            pass=pass:cli
-        ''',
-)
+        name='ngen',
+        version='0.1.1',
+        description='CLI to talk to keepasscx-cli',
+        long_description=readme,
+        author='Boris Quiroz',
+        author_email='borisquiroz@pm.me',
+        install_requires=['Click'],
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        entry_points={
+            'console_scripts': [
+                'ngen=ngen.cli:cli'
+                ]
+            },
+        )
