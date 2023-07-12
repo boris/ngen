73568720a326b85ae6de0e53e7d6745fded5b15e
Boris Quiroz <borisquiroz@pm.me>
2023-07-11 21:07:26 -0400

    Update requirements and bump version

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ with open('README.rst', encoding='UTF-8') as f:
 
 setup(
         name='ngen',
-        version='0.2.0',
+        version='0.2.1',
         description='CLI to talk to keepasscx-cli',
         long_description=readme,
         author='Boris Quiroz',
         author_email='borisquiroz@pm.me',
-        install_requires=['Click'],
+        install_requires=[
+            'Click',
+            'PyYAML',
+        ],
         packages=find_packages('src'),
         package_dir={'': 'src'},
         entry_points={
